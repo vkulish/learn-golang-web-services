84f6c72483655141bcee08a34c630ee5c2813949
Vladimir Kulish <v.kulish@vk.team>
2025-08-02 16:13:56 +0300

    Week 2 original files

---
+++ 2/99_hw/signer/signer.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"sync"
+)
+
+// сюда писать код
+
+func ExecutePipeline(jobs ...job) {
+	var input chan interface{}
+	var output chan interface{}
+	wg := &sync.WaitGroup{}
+	for i, j := range jobs {
+		output = make(chan interface{}, 100)
+		wg.Add(1)
+		go func(j job, num int, in, out chan interface{}, wg *sync.WaitGroup) {
+			defer wg.Done()
+			defer close(out)
+			j(in, out)
+		}(j, i, input, output, wg)
+
+		input = output
+	}
+
+	wg.Wait()
+}
+
+func SingleHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	for v := range in {
+		data := fmt.Sprintf("%v", v)
+		md5 := DataSignerMd5(data)
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, data, md5 string, out chan interface{}) {
+			wg.Done()
+			a := DataSignerCrc32(data)
+			b := DataSignerCrc32(md5)
+			out <- fmt.Sprintf("%s~%s", a, b)
+		}(wg, data, md5, out)
+	}
+	wg.Wait()
+}
+
+func MultiHash(in, out chan interface{}) {
+	for v := range in {
+		data := fmt.Sprintf("%v", v)
+		wg := &sync.WaitGroup{}
+		var result [6]string
+		for i := range 6 {
+			wg.Add(1)
+			go func(i int, data string, result *[6]string) {
+				defer wg.Done()
+				result[i] = DataSignerCrc32(fmt.Sprintf("%v%v", i, data))
+			}(i, data, &result)
+		}
+		wg.Wait()
+		out <- strings.Join(result[:], "")
+	}
+}
+
+func CombineResults(in, out chan interface{}) {
+	data := make([]string, 0, 100)
+	for v := range in {
+		data = append(data, fmt.Sprintf("%v", v))
+	}
+	
+	sort.Slice(data, func(i, j int) bool {
+		return data[i] < data[j]
+	})
+
+	out <- strings.Join(data, "_")
+}
