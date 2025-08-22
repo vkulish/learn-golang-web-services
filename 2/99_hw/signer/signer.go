1d77eb76fba85b851b88c262f41d1b2dc46e8f4c
Vladimir Kulish <v.kulish@vk.team>
2025-08-22 19:52:34 +0300

    PASSED: execition takes less then 3 sec

--- 2/99_hw/signer/signer.go
+++ 2/99_hw/signer/signer.go
@@ -13,14 +13,14 @@ func ExecutePipeline(jobs ...job) {
 	var input chan any
 	var output chan any
 	wg := &sync.WaitGroup{}
-	for i, j := range jobs {
+	for _, j := range jobs {
 		output = make(chan any, 100)
 		wg.Add(1)
-		go func(j job, num int, in, out chan any, wg *sync.WaitGroup) {
+		go func(j job, in, out chan any, wg *sync.WaitGroup) {
 			defer wg.Done()
 			defer close(out)
 			j(in, out)
-		}(j, i, input, output, wg)
+		}(j, input, output, wg)
 
 		input = output
 	}
@@ -28,37 +28,61 @@ func ExecutePipeline(jobs ...job) {
 	wg.Wait()
 }
 
-func SingleHash(in, out chan any) {
+func SingleHash(in chan any, out chan any) {
 	wg := &sync.WaitGroup{}
+	mtx := &sync.Mutex{}
 	for v := range in {
-		data := fmt.Sprintf("%v", v)
-		md5 := DataSignerMd5(data)
 		wg.Add(1)
-		go func(wg *sync.WaitGroup, data, md5 string, out chan any) {
+		go func(wg *sync.WaitGroup, out chan any) {
 			defer wg.Done()
-			a := DataSignerCrc32(data)
-			b := DataSignerCrc32(md5)
+
+			data := fmt.Sprintf("%v", v)
+
+			// To prevent from overheating guard call to
+			// DataSignerMd5() from multithreading
+			mtx.Lock()
+			md5 := DataSignerMd5(data)
+			mtx.Unlock()
+
+			wg2 := &sync.WaitGroup{}
+			var a, b string
+			wg2.Add(2)
+			go func() {
+				defer wg2.Done()
+				a = DataSignerCrc32(data)
+			}()
+			go func() {
+				defer wg2.Done()
+				b = DataSignerCrc32(md5)
+			}()
+			wg2.Wait()
 			out <- fmt.Sprintf("%s~%s", a, b)
-		}(wg, data, md5, out)
+		}(wg, out)
 	}
 	wg.Wait()
 }
 
 func MultiHash(in, out chan any) {
+	wg := &sync.WaitGroup{}
 	for v := range in {
-		data := fmt.Sprintf("%v", v)
-		wg := &sync.WaitGroup{}
-		var result [6]string
-		for i := range 6 {
-			wg.Add(1)
-			go func(i int, data string, result *[6]string) {
-				defer wg.Done()
-				result[i] = DataSignerCrc32(fmt.Sprintf("%v%v", i, data))
-			}(i, data, &result)
-		}
-		wg.Wait()
-		out <- strings.Join(result[:], "")
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			data := fmt.Sprintf("%v", v)
+			wg2 := &sync.WaitGroup{}
+			var result [6]string
+			for i := range 6 {
+				wg2.Add(1)
+				go func(i int, data string, result *[6]string) {
+					defer wg2.Done()
+					result[i] = DataSignerCrc32(fmt.Sprintf("%v%v", i, data))
+				}(i, data, &result)
+			}
+			wg2.Wait()
+			out <- strings.Join(result[:], "")
+		}()
 	}
+	wg.Wait()
 }
 
 func CombineResults(in, out chan any) {
